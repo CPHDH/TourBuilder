237233cf6f252fb393ec4f716a096c2c57a890fe
Greyson Fischer <greyson@foosoft.us>
2013-09-17 16:56:22 -0400

    Fixed findItemsByTourId tour table query

--- models/Table/Tour.php
+++ models/Table/Tour.php
@@ -2,39 +2,28 @@
 
 class TourTable extends Omeka_Db_Table
 {
-   public function findItemsByTourId( $tour_id )
-   {
-      $db = get_db();
-
-      $itemTable = $this->getTable( 'Item' );
-      $select = $itemTable->getSelect();
-      $iAlias = $itemTable->getTableAlias();
-      $select->joinInner( array( 'ti' => $db->TourItem ),
-         "ti.item_id = $iAlias.id", array() );
-      $select->where( 'ti.tour_id = ?', array( $tour_id ) );
-      $select->order( 'ti.ordinal ASC' );
-
-      $items = $itemTable->fetchObjects( $select );
-      return $items;
-   }
-
    public function getSelect()
    {
       $select = parent::getSelect();
-
-      // Determine public level
-      $acl = Omeka_Context::getInstance()->acl;
-      if( $acl && $acl->has( 'TourBuilder_Tours' ) )
-      {
-         $has_permission = $acl->isAllowed( current_user(), 'TourBuilder_Tours',
-                                            'showNotPublic' );
-         if( ! $has_permission )
-         {
-            $select->where( 't.public = 1' );
-         }
-      }
-
+      $permissions = new Omeka_Db_Select_PublicPermissions( 'TourBuilder_Tours' );
+      $permissions->apply( $select, 'tours', null );
       return $select;
+   }
+
+   public function findItemsByTourId( $tour_id )
+   {
+      $db = $this->getDb();
+
+      $iTable = $this->getTable( 'Item' );
+      $select = $iTable->getSelect();
+      $iAlias = $iTable->getTableAlias();
+      $select->joinInner( array( 'tour_item' => $db->TourItem ),
+                          "tour_item.item_id = $iAlias.id");
+      $select->where( 'tour_item.tour_id = ?' );
+      $select->order( 'tour_item.ordinal ASC' );
+
+      $items = $this->fetchObjects( $select, array( $tour_id ) );
+      return $items;
    }
 
 }
