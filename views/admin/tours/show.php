6ac47bd16b3ef6f8d559f0558e299bf5f9b5deba
Greyson Fischer <greyson@foosoft.us>
2013-10-28 17:13:40 -0400

    Cleanup

--- views/admin/tours/show.php
+++ views/admin/tours/show.php
@@ -13,53 +13,51 @@ echo flash();
 ?>
 
 <section class="seven columns alpha">
-<div id="primary">
-   <?php if( $tour->slug ): ?>
-   <div id="tour-slug" class="element">
-      <h2>Slug</h2>
-      <div class="element-text">
-         <?php echo tour( 'Slug' ); ?>
-      </div>
-   </div>
-   <?php endif; ?>
+  <?php if( $tour->slug ): ?>
+  <div id="tour-slug" class="element">
+    <h2>Slug</h2>
+    <div class="element-text">
+      <?php echo tour( 'Slug' ); ?>
+    </div>
+  </div>
+  <?php endif; ?>
 
-   <?php if( metadata( 'tour', 'Description' ) ): ?>
-   <div id="tour-description" class="element">
-      <h2>Description</h2>
-      <div class="element-text">
-         <?php echo nls2p( metadata( 'tour', 'Description' ) ); ?>
-      </div>
-   </div>
-   <?php endif; ?>
+  <?php if( metadata( 'tour', 'Description' ) ): ?>
+  <div id="tour-description" class="element">
+    <h2>Description</h2>
+    <div class="element-text">
+      <?php echo nls2p( metadata( 'tour', 'Description' ) ); ?>
+    </div>
+  </div>
+  <?php endif; ?>
 
-   <?php if( metadata( 'tour', 'Credits' ) ): ?>
-   <div id="tour-credits" class="element">
-      <h2>Credits</h2>
-      <div class="element-text">
-         <?php echo metadata( 'tour', 'Credits' ); ?>
-      </div>
-   </div>
-   <?php endif; ?>
+  <?php if( metadata( 'tour', 'Credits' ) ): ?>
+  <div id="tour-credits" class="element">
+    <h2>Credits</h2>
+    <div class="element-text">
+      <?php echo metadata( 'tour', 'Credits' ); ?>
+    </div>
+  </div>
+  <?php endif; ?>
 
-   <?php
-     $items = $tour->getItems();
-     if( $tour->getItems() ): ?>
-   <div id="tour-items" class="element">
-     <h2>Items</h2>
-     <div class="element-text">
-       <ul>
-         <?php foreach( $items as $item ):
-           set_current_record( 'item', $item, true );
-         ?>
-         <li>
-           <?php echo link_to_item(); ?>
-         </li>
-         <?php endforeach; ?>
+  <?php
+    $items = $tour->getItems();
+    if( $tour->getItems() ): ?>
+  <div id="tour-items" class="element">
+    <h2>Items</h2>
+    <div class="element-text">
+      <ul>
+        <?php foreach( $items as $item ):
+          set_current_record( 'item', $item, true );
+        ?>
+        <li>
+          <?php echo link_to_item(); ?>
+        </li>
+        <?php endforeach; ?>
       </ul>
-   </div>
-   <?php endif; ?>
-</div>
-</div>
+    </div>
+    <?php endif; ?>
+  </div>
 </section>
 
 <section class="three columns omega">
@@ -76,14 +74,11 @@ echo flash();
       <?php echo __('View Public Page'); ?>
     </a>
 
-    <?php
-      if( is_allowed( 'TourBuilder_Tours', 'delete' ) )
-      {
-         echo link_to_tour( __('Delete'),
+    <?php if( is_allowed( 'TourBuilder_Tours', 'delete' ) ): ?>
+    <?php echo link_to_tour( __('Delete'),
                             array( 'class' => 'big red button' ),
-                            'delete-confirm' );
-      }
-    ?>
+                            'delete-confirm' ); ?>
+    <?php endif; ?>
   </div>
 
   <div class="public-featured panel">
