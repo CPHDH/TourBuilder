4f4a728bc222c0be597f23e989cf5367e27bf186
Erin Bell <ebellempire@gmail.com>
2014-11-03 14:13:29 -0500

    add helper text to form

--- views/admin/tours/metadata-form.php
+++ views/admin/tours/metadata-form.php
@@ -7,6 +7,7 @@
  	</div>
  	<div class="five columns omega">
  	  <?php echo $this->formText( 'title', $tour->title ); ?>
+ 	  <p class="explanation"><?php echo __('A title for the tour.');?></p>
  	</div>
    </div>
 
@@ -16,6 +17,7 @@
  	</div>
  	<div class="five columns omega">
  	  <?php echo $this->formText( 'slug', $tour->slug ); ?>
+ 	  <p class="explanation"><?php echo __('A short string of alphanumeric characters (no punctuation) used to identify the tour.');?></p>
  	</div>
    </div>
 
@@ -25,6 +27,7 @@
  	</div>
  	<div class="five columns omega inputs">
  	  <?php echo $this->formText( 'credits', $tour->credits ); ?>
+ 	  <p class="explanation"><?php echo __('The name of the person(s) or organization responsible for the content of the tour.');?></p>
  	</div>
    </div>
      
@@ -34,9 +37,17 @@
          </div>
         <div class="five columns omega inputs">
             <?php echo $this->formFile('image'); ?>
+            
+             <p class="explanation">
+	             <?php echo __('A single image file used to represent the tour in mobile apps.');?>
+	         </p>           
+            
             <?php
             if($tour->hasImage()) {
-                echo '<br><i>'.__('The tour image will only be overwritten if you select a new file.').'</i>';
+	            
+                echo '<div id="admin-tour-image">'.$tour->image().'</div>';
+                echo '<span class="file-helper">'.__('The tour image will only be overwritten if you select a new file.').'</span>';
+           
             }
             ?>
         </div>
@@ -45,10 +56,12 @@
    <div class="field">
  	<div class="two columns alpha">
  	  <?php echo $this->formLabel( 'description', __('Description') ); ?>
+ 	  
  	</div>
  	<div class="five columns omega inputs">
  	  <?php echo $this->formTextarea( 'description', $tour->description,
  		                              array( 'rows' => 8, 'cols' => '40' ) ); ?>
+ 		                              <p class="explanation"><?php echo __('The main text of the tour.');?></p>
  	</div>
    </div>
  </fieldset>
