a2e27ce392715e66265abfed2889888321cc5732
Erin Bell <campus@dyn-host-137-148-121-25.csuohio.edu>
2017-07-13 11:56:06 -0400

    misc/minor

--- controllers/ToursController.php
+++ controllers/ToursController.php
@@ -8,23 +8,5 @@ class TourBuilder_ToursController extends Omeka_Controller_AbstractActionControl
 	{
 		$this->_helper->db->setDefaultModelName( 'Tour' );
 	}
-
-	public function savetouritemsAction() {
-		$tour = $this->_helper->db->findById();
-
-		# Remove all of the items in the tour
-		$tour->removeAllItems();
-
-		# Get our POST of the saveOrder
-		$post = $this->getRequest()->getPost();
-		$aOrder = json_decode($post['saveOrder'],true);
-
-		# Iterate through all of the tour items
-		# passed in an add them to the tour
-		for($i = 0; $i < count($aOrder); $i++) {
-			$item_id = intval( $aOrder[$i] );
-			$tour->addItem( $item_id, $i );
-		}
-	}
 	
 }
